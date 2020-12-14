0f3adb9e19ee6ee09dff7baf45f7d3eb291ccebb
Harshit Mandada <mandada.h@husky.neu.edu>
2020-12-14 22:43:40 +0530

    client

---
+++ client/build/precache-manifest.a62f53f9ad2ba3f458cc872a88976409.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0971e325db9bc08eb7f05d778be560f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54e85e01193ced4c0403",
+    "url": "/static/css/main.f5d51f30.chunk.css"
+  },
+  {
+    "revision": "630739c3cf67ad617be9",
+    "url": "/static/js/2.0f9e45f8.chunk.js"
+  },
+  {
+    "revision": "54e85e01193ced4c0403",
+    "url": "/static/js/main.73f859f1.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "810f706cae4a8901415f08590d5e4277",
+    "url": "/static/media/node.810f706c.png"
+  },
+  {
+    "revision": "6ed3fceeb4b45c3dd1c85dcf0d2e5f6d",
+    "url": "/static/media/react.6ed3fcee.png"
+  }
+]);
